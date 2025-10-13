9067c74ee38070aeaad0724c8c3cc3e130b682f0
Yahya-Aboutaya <yahyaaboutaya@gmail.com>
2025-10-13 16:19:55 +0200

    Initial commit

---
+++ src/Entity/Instrument.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InstrumentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstrumentRepository::class)]
+class Instrument
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $numSerie = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateAchat = null;
+
+    #[ORM\Column]
+    private ?bool $prixAchat = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $utilisation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cheminImage = null;
+
+    #[ORM\ManyToOne(inversedBy: 'instruments')]
+    private ?Marque $instrument = null;
+
+    #[ORM\OneToMany(targetEntity: Intervention::class, mappedBy: 'instrument')]
+    private Collection $interventions;
+
+    #[ORM\OneToMany(targetEntity: ContratPret::class, mappedBy: 'instrument')]
+    private Collection $contratPrets;
+
+    public function __construct()
+    {
+        $this->interventions = new ArrayCollection();
+        $this->contratPrets = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumSerie(): ?int
+    {
+        return $this->numSerie;
+    }
+
+    public function setNumSerie(int $numSerie): static
+    {
+        $this->numSerie = $numSerie;
+
+        return $this;
+    }
+
+    public function getDateAchat(): ?\DateTimeInterface
+    {
+        return $this->dateAchat;
+    }
+
+    public function setDateAchat(\DateTimeInterface $dateAchat): static
+    {
+        $this->dateAchat = $dateAchat;
+
+        return $this;
+    }
+
+    public function isPrixAchat(): ?bool
+    {
+        return $this->prixAchat;
+    }
+
+    public function setPrixAchat(bool $prixAchat): static
+    {
+        $this->prixAchat = $prixAchat;
+
+        return $this;
+    }
+
+    public function getUtilisation(): ?string
+    {
+        return $this->utilisation;
+    }
+
+    public function setUtilisation(string $utilisation): static
+    {
+        $this->utilisation = $utilisation;
+
+        return $this;
+    }
+
+    public function getCheminImage(): ?string
+    {
+        return $this->cheminImage;
+    }
+
+    public function setCheminImage(?string $cheminImage): static
+    {
+        $this->cheminImage = $cheminImage;
+
+        return $this;
+    }
+
+    public function getInstrument(): ?Marque
+    {
+        return $this->instrument;
+    }
+
+    public function setInstrument(?Marque $instrument): static
+    {
+        $this->instrument = $instrument;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Intervention>
+     */
+    public function getInterventions(): Collection
+    {
+        return $this->interventions;
+    }
+
+    public function addIntervention(Intervention $intervention): static
+    {
+        if (!$this->interventions->contains($intervention)) {
+            $this->interventions->add($intervention);
+            $intervention->setInstrument($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIntervention(Intervention $intervention): static
+    {
+        if ($this->interventions->removeElement($intervention)) {
+            // set the owning side to null (unless already changed)
+            if ($intervention->getInstrument() === $this) {
+                $intervention->setInstrument(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ContratPret>
+     */
+    public function getContratPrets(): Collection
+    {
+        return $this->contratPrets;
+    }
+
+    public function addContratPret(ContratPret $contratPret): static
+    {
+        if (!$this->contratPrets->contains($contratPret)) {
+            $this->contratPrets->add($contratPret);
+            $contratPret->setInstrument($this);
+        }
+
+        return $this;
+    }
+
+    public function removeContratPret(ContratPret $contratPret): static
+    {
+        if ($this->contratPrets->removeElement($contratPret)) {
+            // set the owning side to null (unless already changed)
+            if ($contratPret->getInstrument() === $this) {
+                $contratPret->setInstrument(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
