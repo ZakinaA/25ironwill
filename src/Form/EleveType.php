b7dcae6b024f094b065b8249badfcb24a5fb9563
sio2 <marielucas665@gmail.com>
2025-10-13 17:00:43 +0200

    Ajout controller Eleve

---
+++ src/Form/EleveType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Eleve;
+use App\Entity\Responsable;
+use App\Entity\Tranche;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EleveType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('numRue')
+            ->add('rue')
+            ->add('copos')
+            ->add('ville')
+            ->add('tel')
+            ->add('mail')
+            ->add('responsables', EntityType::class, [
+                'class' => Responsable::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+            ->add('tranche', EntityType::class, [
+                'class' => Tranche::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Eleve::class,
+        ]);
+    }
+}
