380f46595df7ae9ed26f4b04eabd10495900e232
sio2 <noelgaultiercamille@gmail.com>
2025-11-03 14:33:59 +0100

    Amelioration de la page inscription a un cours(ajout d'un filtre pour chercher un eleve + le template) il reste a faire choix multiple pour les cours + gerer les paiements

--- src/Controller/InscriptionCoursController.php
+++ src/Controller/InscriptionCoursController.php
@@ -14,13 +14,12 @@ use Symfony\Component\HttpFoundation\Request;
 class InscriptionCoursController extends AbstractController
 {
     #[Route('/inscription_cours', name: 'app_inscription_cours')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+public function index(Request $request, EntityManagerInterface $em): Response
 {
-    // Récupérer les IDs envoyés par le formulaire
-    $eleveId = $request->request->get('eleve'); // si formulaire POST
+    $eleveId = $request->request->get('eleve');
     $coursId = $request->request->get('cours');
 
-    if ($eleveId && $coursId) {
+    if ($request->isMethod('POST') && $eleveId && $coursId) {
         $eleve = $em->getRepository(Eleve::class)->find($eleveId);
         $cours = $em->getRepository(Cours::class)->find($coursId);
 
@@ -34,13 +33,17 @@ class InscriptionCoursController extends AbstractController
             $em->flush();
 
             $this->addFlash('success', 'Inscription réussie !');
+
+            // 🔹 REDIRECTION pour éviter le double POST
+            return $this->redirectToRoute('app_inscription_cours');
         }
     }
 
-    return $this->render('pages/inscription_cours/index.html.twig', [
-    'controller_name' => 'InscriptionCoursController',
-    'eleves' => $em->getRepository(Eleve::class)->findAll(),
-    'cours' => $em->getRepository(Cours::class)->findAll(),
-]);
+    return $this->render('inscription_cours/index.html.twig', [
+        'eleves' => $em->getRepository(Eleve::class)->findAll(),
+        'cours' => $em->getRepository(Cours::class)->findAll(),
+    ]);
+}
 
-}}
+
+}
