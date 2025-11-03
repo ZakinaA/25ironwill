a686fb8a1d6791cc703c577473fef0c9dedce1fd
sio2 <noelgaultiercamille@gmail.com>
2025-11-03 15:58:11 +0100

    Ajout du style debut avancement sur l'ajout des responsable qui sont en commentaire pour eviter tout conflicts

--- src/Controller/InscriptionCoursController.php
+++ src/Controller/InscriptionCoursController.php
@@ -7,34 +7,40 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Eleve;
 use App\Entity\Cours;
+use App\Entity\Responsable;
 use App\Entity\Inscription;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class InscriptionCoursController extends AbstractController
 {
-    #[Route('/inscription_cours', name: 'app_inscription_cours')]
+   #[Route('/inscription_cours', name: 'app_inscription_cours')]
 public function index(Request $request, EntityManagerInterface $em): Response
 {
     $eleveId = $request->request->get('eleve');
-    $coursId = $request->request->get('cours');
+    $coursIds = $request->request->all('cours'); 
+    $responsableIds = $request->request->all('responsable');
 
-    if ($request->isMethod('POST') && $eleveId && $coursId) {
+    if ($request->isMethod('POST') && $eleveId && !empty($coursIds) && !empty($responsableIds)) {
         $eleve = $em->getRepository(Eleve::class)->find($eleveId);
-        $cours = $em->getRepository(Cours::class)->find($coursId);
 
-        if ($eleve && $cours) {
-            $inscription = new Inscription();
-            $inscription->setEleve($eleve);
-            $inscription->setCours($cours);
-            $inscription->setDateInscription(new \DateTime());
+        if ($eleve) {
+            foreach ($coursIds as $coursId) {
+                $cours = $em->getRepository(Cours::class)->find($coursId);
+                if ($cours) {
+                    $inscription = new Inscription();
+                    $inscription->setEleve($eleve);
+                    $inscription->setCours($cours);
+                    $inscription->setResponsable($responsable);
+                    $inscription->setDateInscription(new \DateTime());
 
-            $em->persist($inscription);
+                    $em->persist($inscription);
+                }
+            }
+
             $em->flush();
+            $this->addFlash('success', 'Inscriptions réussies !');
 
-            $this->addFlash('success', 'Inscription réussie !');
-
-            // 🔹 REDIRECTION pour éviter le double POST
             return $this->redirectToRoute('app_inscription_cours');
         }
     }
@@ -42,8 +48,10 @@ public function index(Request $request, EntityManagerInterface $em): Response
     return $this->render('inscription_cours/index.html.twig', [
         'eleves' => $em->getRepository(Eleve::class)->findAll(),
         'cours' => $em->getRepository(Cours::class)->findAll(),
+        'responsable' => $em->getRepository(Responsable::class)->findAll(),
     ]);
 }
+
 
 
 }
