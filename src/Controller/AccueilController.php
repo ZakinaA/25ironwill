3e6b9f9609d9f9bc6ab65c5b1d2a83a45452b8c2
sio2 <marielucas665@gmail.com>
2025-11-03 14:46:20 +0100

    Ajout de la liste des cours

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Cours;
+use App\Form\CoursType;
+use App\Repository\CoursRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function accueil(): Response
     {
-        return $this->render('accueil.html.twig', [
+        return $this->render('accueil/accueil.html.twig', [
             'title' => 'Bienvenue',
+        ]);
+    }
+
+    #[Route('/accueil-cours', name: 'app_accueil_cours', methods: ['GET'])]
+    public function cours(CoursRepository $coursRepository): Response
+    {
+        return $this->render('accueil/cours.html.twig', [
+            'cours' => $coursRepository->findAll(),
         ]);
     }
 }
