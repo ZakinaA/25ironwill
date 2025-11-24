3dd069981dba58586c11aeb741e5c45de7dd6a64
sio2 <marielucas665@gmail.com>
2025-11-24 14:50:41 +0100

    correction des bug majeurs

--- migrations/Version20251124131627.php
+++ migrations/Version20251124131627.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251124131520 extends AbstractMigration
+final class Version20251124131627 extends AbstractMigration
 {
     public function getDescription(): string
     {
