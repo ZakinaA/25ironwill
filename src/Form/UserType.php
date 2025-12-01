faf93e84d649d4e89e52bc9a2b20841b8ace5674
Yahya-Aboutaya <yahyaaboutaya@gmail.com>
2025-12-01 16:52:31 +0100

    Ajout du boutton Utilisateurs sur la sidebarre création de son crud et modifoication du style

---
+++ src/Form/UserType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use App\Entity\Eleve;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true, // cases à cocher
+            ])
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('numRue')
+            ->add('rue')
+            ->add('copos')
+            ->add('ville')
+            ->add('tel')
+            ->add('eleve', EntityType::class, [
+                'class' => Eleve::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
